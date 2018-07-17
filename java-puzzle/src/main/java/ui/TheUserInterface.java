0e1063f4e10b6a6727026865a5425f6df33092b0
masengwa <me.pius1102@gmail.com>
2018-07-17 21:23:01 +0300

    Fixing The Listener

--- java-puzzle/src/main/java/ui/TheUserInterface.java
+++ java-puzzle/src/main/java/ui/TheUserInterface.java
@@ -10,6 +10,8 @@ import java.awt.event.ActionListener;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 public class TheUserInterface extends JFrame implements ActionListener {
 
@@ -178,7 +180,7 @@ public class TheUserInterface extends JFrame implements ActionListener {
 
         Uncomment this piece of code if You Want To Launch The game with all pieces
         correctly arranged. Before That You Should Comment lines 126 t0 133*/
-        //buttons[i][j] = new JButton(getIcons().get(value));
+        //btnImages[i][j] = new JButton(getIcons().get(value));
 
       }
     }
@@ -212,7 +214,7 @@ public class TheUserInterface extends JFrame implements ActionListener {
   }
 
   /**
-   * This method registers the action listener to all buttons
+   * This method registers the action listener to all btnImages
    */
   private void registerButtonsListener() {
     for (int i = 0; i <getGameSize() ; i++) {
@@ -234,6 +236,158 @@ public class TheUserInterface extends JFrame implements ActionListener {
 
   @Override
   public void actionPerformed(ActionEvent e) {
+    Index2D pressedIndex = getPressedButtonIndices(e,getButtons());
+    assert pressedIndex != null;
+    int pressedXIndex = pressedIndex.getXIndex();
+    int pressedYIndex = pressedIndex.getYIndex();
+
+    //System.out.println("Pressed Button Index-->["+pressedIndex.getXIndex()+","+pressedIndex.getYIndex()+"]");
+
+    if(new Index2D().compareIndex2D(pressedIndex,getKeyIndex())){
+      error();
+    }else{
+
+      //Check Nearby Indices
+      ArrayList<Index2D> nearIndices
+              = new Index2D().nearbyIndices(getGameSize(),pressedIndex);
+      //Print Nearby Indices
+      // nearIndices.forEach(index->new Index2D().printIndex(index));
+
+      /*
+       * Option<Index2D> lst below equal null if the nearby indices do not
+       * contains Index2D that resembles the key Index2D which is actually
+       * the key button itself
+       */
+      Optional<Index2D> lst = nearIndices.stream()
+              .filter(p->new Index2D().compareIndex2D(p,getKeyIndex())).findFirst();
+      Index2D theKey  = lst.orElse(null);
+
+      /*
+        Option<Index2D> lst above contains either null or 1 Index2D value which
+        is the index of the key button.
+        If it is not null then the move (ImageIcon swapping) is allowed.
+        Index of the key is then updated.
+       */
+
+      if(theKey!=null){
+        new Index2D().printIndex(theKey);
+        JButton b = (JButton) e.getSource();
+        ImageIcon tempIcon = (ImageIcon) b.getIcon();
+        b.setIcon(null);
+        btnImages[getKeyIndex().getXIndex()][getKeyIndex().getYIndex()].setIcon(tempIcon);
+        setKeyIndex(new Index2D(pressedXIndex,pressedYIndex));
+        new Index2D().printIndex(getKeyIndex());
+
+      }else {
+        error();
+      }
+    }
+
+    /*
+        Checking The Game Completion
+        If the game is complete
+              -->Embed a last piece of the Image
+              -->Freeze (Disable) all neighbouring Indices2D
+              -->Show A Pop Up Message
+     */
+
+    if(new Index2D().compareIndex2D(pressedIndex,new Index2D((getGameSize()-1),(getGameSize()-1)))){
+      if(isGameComplete()){
+        System.out.println("[COMPLETE]");
+
+        btnImages[getGameSize()-1][getGameSize()-1].setIcon(new ImageIcon(Objects.requireNonNull(classLoader.getResource("img/row-4-col-4.jpg"))));
+        //Freeze Neighbours
+        //int x=getGameSize()-1;
+        //int x1 = x-2;
+        btnImages[2][3].setEnabled(false);
+        btnImages[3][2].setEnabled(false);
+        showGameOver();
+      }else{
+        System.out.println("[NOT COMPLETE]");
+      }
+    }
 
   }
+
+
+  /**
+   * This Method is called every time the last Index2D is pressed and checks if it was a final move
+   * @return true if the game is complete and false if not
+   */
+
+  private boolean isGameComplete(){
+
+    boolean a= true;
+
+    int _1DIndex;
+    for (int i = 0; i <getGameSize(); i++) {
+      for (int j = 0; j <getGameSize() ; j++) {
+        _1DIndex=getGameSize()*i + j;
+
+        ImageIcon icn = (ImageIcon) getButton(i,j).getIcon();
+        ImageIcon icn2 = getIcons().get(_1DIndex);
+
+        if(i==getGameSize()-1 && j==getGameSize()-1){
+          continue;
+        }
+
+        if(!(icn.equals(icn2))){
+          a=false;
+        }
+
+      }
+    }
+    return a;
+  }
+
+  /**
+   * This method is called to identify the position of the pressed button(in Index2D format)
+   * @param e ActionEvent of the button pressed
+   * @param btnImages pressed button
+   * @return Index2D value of the pressed button in a matrix
+   */
+  private Index2D getPressedButtonIndices(ActionEvent e, JButton [][] btnImages){
+    for (int i = 0; i <getGameSize() ; i++) {
+      for (int j = 0; j <getGameSize(); j++) {
+        if(e.getSource()==btnImages[i][j]){
+          return  new Index2D(i,j);
+        }
+      }
+    }
+    return null;
+  }
+
+  /**
+   * When called this method sound a default OS error beep.
+   */
+  private void error(){
+    Toolkit.getDefaultToolkit().beep();
+  }
+
+  /**
+   * This Method launches a JDialog its called when game is over
+   */
+
+  private void showGameOver() {
+
+    Object[] options = {"RESTART","EXIT"};
+    int n = JOptionPane.showOptionDialog(null,"WELL DONE!!",
+            "GAME IS OVER",
+            JOptionPane.YES_NO_OPTION,
+            JOptionPane.INFORMATION_MESSAGE,
+            null,
+            options,
+            options[1]);
+    if(n==JOptionPane.YES_OPTION) {
+      new TheUserInterface(getGameSize());
+      setVisible(true);
+      this. dispose();
+    }
+
+    if (n==JOptionPane.NO_OPTION) {
+      System.exit(0);
+    }
+
+  }//end method showGameOver
+
 }
